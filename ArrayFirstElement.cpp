5d522d4bbd914ef3ea3154439dabdf18fa0c066c
Aarav <aarav25803691@gmail.com>
2024-07-25 20:49:57 -0500

    ArrayFirstElement.cpp (CORRECT)

--- ArrayFirstElement.cpp
+++ ArrayFirstElement.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main(){
     int firstElement[2] = {1, 2};
-    std::cout<<firstElement[1];
-    return firstElement[1];
+    std::cout<<firstElement[0];
+    return firstElement[0];
 }
