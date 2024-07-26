4ab69404f727cbdd17d5b27619feb4c90a1902b2
Aarav <aarav25803691@gmail.com>
2024-07-26 12:24:51 -0500

    ReverseArray.cpp

--- ReverseArray.cpp
+++ ReverseArray.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
 using namespace std;
-
-int* reverse(int list[], int size) {
-    // Create a new array to hold the reversed elements
-    int* reversed = new int[size];
-    
-    // Fill the reversed array with elements from the original array in reverse order
-    for (int i = 0; i < size; i++) {
-        reversed[i] = list[size - 1 - i];
+void repeat(int list[], int size){
+    for (int i=size-1; i>=0; i--){
+        cout<<list[i]<<" ";
     }
-    
-    return reversed;
 }
-
-int main() {
-    int list[] = {1, 2, 3};
-    int size = sizeof(list) / sizeof(list[0]);
-
-    // Call the reverse function
-    int* reversedList = reverse(list, size);
-
-    // Print the reversed array
-    cout << "Reversed array: [";
-    for (int i = 0; i < size; i++) {
-        cout << reversedList[i];
-        if (i < size - 1) cout << ", ";
-    }
-    cout << "]" << endl;
-
-    // Free the allocated memory
-    delete[] reversedList;
-
-    return 0;
+int main(){
+    int list1[] = {1, 2, 5, 2};
+    int size1 = sizeof(list1) / sizeof(list1[0]);
+    repeat(list1, size1);
 }
